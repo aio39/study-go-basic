1dcac9d8c4dd8efdae2c49b12b59536eae7046d0
Aio <AioDev.JS@gmail.com>
2022-06-29 11:37:38 +0900

    Function

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int { // a int, b int  // Type이 같은 경우는 한번만 작성하면 됨
+	return a * b
+}
+
+// func lenAndUpper(name string) ( int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+// naked return
+func lenAndUpper(name string) (length int, upperCase string) {
+	defer fmt.Println("I'm done") // 함수가 끝나고 실
+	length = len(name)
+	upperCase = strings.ToUpper(name)
+	return
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	const name string = "aio39"
-	change := "a" // 타입 추론, 한번 정한 type은 변경 불가능. func 안에서만 가능  ==> var change string = "39"
-	change = "b"
-	// name = "aio40"
-	fmt.Println(name)
-	fmt.Println(change)
+	// fmt.Println(multiply(2, 3))
+	totalLength, upperName := lenAndUpper("Hello") // _로 받으면 ignored value가
+	fmt.Println(totalLength, upperName)
+	repeatMe("aio", "miku", "ichika", "dare")
 }
