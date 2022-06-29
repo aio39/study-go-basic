8c6200ac1939101bbff45e18d91d5b3bf7457833
Aio <AioDev.JS@gmail.com>
2022-06-29 11:43:39 +0900

    for loop

--- main.go
+++ main.go
@@ -1,33 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func multiply(a, b int) int { // a int, b int  // Type이 같은 경우는 한번만 작성하면 됨
-	return a * b
-}
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers { // range는 for하고만 쓰이는 문법
+		// fmt.Println(index, number)
+		total += number
+	}
 
-// func lenAndUpper(name string) ( int, string) {
-// 	return len(name), strings.ToUpper(name)
-// }
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
 
-// naked return
-func lenAndUpper(name string) (length int, upperCase string) {
-	defer fmt.Println("I'm done") // 함수가 끝나고 실
-	length = len(name)
-	upperCase = strings.ToUpper(name)
-	return
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+	return total
 }
 
 func main() {
-	// fmt.Println(multiply(2, 3))
-	totalLength, upperName := lenAndUpper("Hello") // _로 받으면 ignored value가
-	fmt.Println(totalLength, upperName)
-	repeatMe("aio", "miku", "ichika", "dare")
+	total := superAdd(1, 2, 3, 4, 5)
+	fmt.Println(total)
 }
