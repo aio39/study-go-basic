8602144ca9c8d55561e231b0ad4db930b6d5f08d
Aio <AioDev.JS@gmail.com>
2022-07-01 22:44:22 +0900

    Maps & Structs

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+} // go에는 __init__, super(), constructor() 등이 없
+
 func main() {
-	names := [5]string{"aio", "miku", "rin"} // Array
-	names[3] = "3"
-	names[4] = "4"
+	aio := map[string]string{"name": "aio", "age": "17"} // key - value
 
-	infinite := []string{} // Slice
-	infinite = append(infinite, "aio")
+	fmt.Println(aio)
 
-	fmt.Println(names, infinite)
+	for key, value := range aio {
+		fmt.Println(key, value)
+	}
+	// --------------------------
+
+	favFood := []string{"chicken", "chocolateMintIce"}
+	// aio2 := person{"aio", 17, favFood} -> 가능하지만 가독성이 나쁨,  두가지 방법 Mix해서 사용 불가
+	aio2 := person{name: "aio", age: 17, favFood: favFood}
+	fmt.Println(aio2, aio2.age)
 
 }
