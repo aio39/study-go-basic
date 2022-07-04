acebd8c0aea68d555e74ae79f85733f439ca47b9
Aio <AioDev.JS@gmail.com>
2022-07-04 19:31:31 +0900

    Method, Receiver

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -12,3 +12,19 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) { // receiver, 첫 글자는 Struct의 첫 글자
+	// *를 해주지 않으면 복사복의 Account를 받아온다. * -> pointer receiver
+	a.balance += amount // go에서는 Class(struct)에서 메서드를 선언하지 않고 이런식으로 함.
+}
+
+// Balance of  your account's balance
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) {
+	a.balance -= amount
+}
--- main.go
+++ main.go
@@ -9,5 +9,7 @@ import (
 func main() {
 	// account := banking.Account{Owner: "aio", Balance: 1000}
 	account := accounts.NewAccount("aio") // construct가 없는 go에서 private 프로퍼티를 초기화 하는 패턴
+	account.Deposit(100)
 	fmt.Println(account)
+	fmt.Println(account.Balance())
 }
