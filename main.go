fb9cbfcc5d5055969e118f9108f0194bb5de35fe
Aio <AioDev.JS@gmail.com>
2022-07-04 20:00:18 +0900

    Type Dict

--- main.go
+++ main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/aio39/study_go_basic/accounts"
+	"github.com/aio39/study_go_basic/mydict"
 )
 
 func main() {
-	// account := banking.Account{Owner: "aio", Balance: 1000}
-	account := accounts.NewAccount("aio") // construct가 없는 go에서 private 프로퍼티를 초기화 하는 패턴
-	account.Deposit(100)
-	err := account.Withdraw(200)
-	if err != nil { // go에서는 에러 Exception을 캐치해주지 않아도 프로그램이 종료되지 않음.
-		// err catch 보다는 귀찮지만, err 체크를 강제하므로 안정적임
-		// log.Fatalln(err)
+	dictionary := mydict.Dictionary{"hello": "world"}
+	dictionary["hello"] = "world"
+	definition, err := dictionary.Search("second")
+	if err != nil {
 		fmt.Println(err)
 	}
-
-	fmt.Println(account.Balance(), account.Owner())
-	fmt.Println(account)
-
+	fmt.Println(definition)
 }
