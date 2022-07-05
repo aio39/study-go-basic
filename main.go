6fe43391d4f33918275ed03d4092226cfa96a080
Aio <AioDev.JS@gmail.com>
2022-07-05 10:39:37 +0900

    Go Routines

--- main.go
+++ main.go
@@ -1,51 +1,20 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 func main() {
-
-	// var results map[string]string  초기화 안됨
-	// var results = map[string]string{}
-	var results = make(map[string]string)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	// fmt.Println(results)
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go cuteCount("aio")
+	cuteCount("miku")
+	// go루틴이 실행중이더라도 main 함수가 종료되면 프로그램이 종료
 }
 
-var errRequestFailed = errors.New("request failed")
-
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+func cuteCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is cute", i)
+		time.Sleep(time.Second)
 	}
-	return nil
+
 }
